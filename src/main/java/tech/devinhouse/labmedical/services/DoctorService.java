78af537badb69d8c9056853d855e6dc27a5b48ac
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-04-18 08:38:01 -0300

    Feat: implements DoctorService

--- src/main/java/tech/devinhouse/labmedical/services/DoctorService.java
+++ src/main/java/tech/devinhouse/labmedical/services/DoctorService.java
@@ -1,23 +1,69 @@
 package tech.devinhouse.labmedical.services;
 
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import tech.devinhouse.labmedical.dtos.DoctorRequest;
 import tech.devinhouse.labmedical.dtos.DoctorResponse;
+import tech.devinhouse.labmedical.entities.DoctorEntity;
+import tech.devinhouse.labmedical.exceptions.DuplicateCpfException;
+import tech.devinhouse.labmedical.exceptions.IllegalBirthdayException;
+import tech.devinhouse.labmedical.exceptions.ImmutableDocumentException;
+import tech.devinhouse.labmedical.exceptions.NoSuchDoctorException;
+import tech.devinhouse.labmedical.mappers.DoctorMapper;
+import tech.devinhouse.labmedical.repositories.DoctorRepository;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Service
 public class DoctorService {
+    private final DoctorMapper mapper;
+    private final DoctorRepository repository;
+
+    public DoctorService(DoctorMapper mapper, DoctorRepository repository) {
+        this.mapper = mapper;
+        this.repository = repository;
+    }
+
     public DoctorResponse register(DoctorRequest request) {
-        // ToDo
-        return null;
+        validateBirthday(request.getBirthday());
+        validateUniqueCPF(request.getCpf());
+
+        DoctorEntity newDoctor = mapper.map(request);
+
+        return mapper.map(repository.save(newDoctor));
+    }
+
+    @SneakyThrows
+    private void validateBirthday(String birthday) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyy");
+        Date birthdate = formatter.parse(birthday);
+        if (birthdate.after(new Date())) throw new IllegalBirthdayException();
+    }
+
+    private void validateUniqueCPF(String cpf) {
+        repository.findByCpf(cpf).ifPresent(e -> {throw new DuplicateCpfException();});
     }
 
     public DoctorResponse update(Integer id, DoctorRequest request) {
-        // ToDo
-        return null;
+        validateBirthday(request.getBirthday());
+
+        DoctorEntity oldDoctor = repository.findById(id).orElseThrow(NoSuchDoctorException::new);
+
+        if (!request.getCpf().equals(oldDoctor.getCpf()) || !request.getRg().equals(oldDoctor.getRg()))
+            throw new ImmutableDocumentException();
+
+        DoctorEntity updatedDoctor = mapper.map(request);
+        updatedDoctor.setId(oldDoctor.getId());
+
+        return mapper.map(repository.save(updatedDoctor));
     }
 
     public DoctorResponse changePassword(Integer id, String password) {
-        // ToDo
-        return null;
+        DoctorEntity doctor = repository.findById(id).orElseThrow(NoSuchDoctorException::new);
+
+        doctor.setPassword(password);
+
+        return mapper.map(repository.save(doctor));
     }
 }

91c6133f301cdabdc9f5d5790a09f90042f5645b
LucasGdosR <109366863+LucasGdosR@users.noreply.github.com>
2023-04-28 12:12:35 -0300

    Refactor: changes validation to AddressRequest

--- src/main/java/tech/devinhouse/labmedical/entities/AddressEntity.java
+++ src/main/java/tech/devinhouse/labmedical/entities/AddressEntity.java
@@ -4,26 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
-import lombok.*;
 
 @Entity
 public class AddressEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @NotBlank
     private String cep;
-    @NotBlank
     private String city;
-    @NotBlank
     private String state;
-    @NotBlank
     private String street;
-    @NotBlank
     private String number;
     private String complement;
-    @NotBlank
     private String neighborhood;
     private String reference;
 
